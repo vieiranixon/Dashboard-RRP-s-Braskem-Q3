38b0ce26829afec4cfaf90de0a631d2747cbc055
vieiranixon <vieiranixon@gmail.com>
2026-07-29 14:34:37 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dashboard-v3";
+const CACHE_NAME = "dashboard-v4";
 
 const urlsToCache = [
   "./",
@@ -13,32 +13,30 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
   );
 });
 
 // Ativa e remove caches antigos
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
         cacheNames.map(cache => {
           if (cache !== CACHE_NAME) {
             return caches.delete(cache);
           }
         })
-      )
-    ).then(() => self.clients.claim())
+      );
+    })
   );
 });
 
 // Busca arquivos
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
